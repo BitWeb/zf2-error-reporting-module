6d79d00fadd0449211acba7b1743b2e441867e96
priit <priit@bitweb.ee>
2014-06-25 17:46:35 +0300

    Send with event

---
+++ src/BitWeb/ErrorReportingModule/ErrorEventManager.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BitWeb\ErrorReportingModule;
+
+
+/**
+ * Created by PhpStorm.
+ * User: priit
+ * Date: 6/25/14
+ * Time: 4:08 PM
+ */
+class ErrorEventManager extends \Zend\EventManager\EventManager implements \BitWeb\ErrorReporting\ErrorEventManager{
+
+}
--- src/BitWeb/ErrorReportingModule/Service/Factory/ErrorServiceFactory.php
+++ src/BitWeb/ErrorReportingModule/Service/Factory/ErrorServiceFactory.php
@@ -10,8 +10,15 @@ namespace BitWeb\ErrorReportingModule\Service\Factory;
 
 
 use BitWeb\ErrorReporting\Service\ErrorService;
+use BitWeb\ErrorReportingModule\ErrorEventManager;
+use BitWeb\Mail\Configuration;
+use BitWeb\Mail\Service\MailService;
+use Zend\Mail\Transport\Sendmail;
+use Zend\Mail\Transport\Smtp;
+use Zend\Mail\Transport\SmtpOptions;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+
 
 class ErrorServiceFactory implements FactoryInterface {
 
@@ -23,8 +30,25 @@ class ErrorServiceFactory implements FactoryInterface {
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $service = new ErrorService($serviceLocator->get('Config')['error_reporting']);
+        $config = $serviceLocator->get('config');
+        if (!isset($config['mail']) || !isset($config['mail']['smtpOptions'])) {
+            $transport = new Sendmail();
+        } else {
+            $transport = new Smtp(new SmtpOptions($config['mail']['smtpOptions']));
+        }
 
+        if (isset($config['mail']) && isset($config['mail']['smtpOptions'])) {
+            unset($config['mail']['smtpOptions']);
+        }
+
+        $configuration = new Configuration($config['mail']);
+        $mailService = new MailService( $transport, $configuration);
+        $errorEventManager = new ErrorEventManager();
+        $mailService->setEventManager($errorEventManager);
+
+        $service = new ErrorService($serviceLocator->get('Config')['error_reporting']);
+        $service->setEventManager($errorEventManager);
+        $service->setEvent( MailService::EVENT_SEND_MAIL);
         return $service;
     }
 
